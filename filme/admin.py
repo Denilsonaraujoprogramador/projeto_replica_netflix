2f6628d987566597e2637dd73e2dd507d6988b07
Denilsonaraujoprogramador <101232538+Denilsonaraujoprogramador@users.noreply.github.com>
2022-07-18 17:23:57 -0300

    Criado a classe de usuario personalizado
    Criado a classe de usuario personalizado no Models.py e criada a variavel "campos" no admin.py para ter a visão no administrador dos filmes vistos

--- filme/admin.py
+++ filme/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Filme, Episodio
+from .models import Filme, Episodio, Usuario
+from django.contrib.auth.admin import UserAdmin
+
+#Esse variavel só existe porque queremos ver no admin o campo personalizado do ususario os filmes_vistos.
+campos = list(UserAdmin.fieldsets)
+campos.append(("Histórico", {'fields': ('filmes_vistos',)}))
+UserAdmin.fieldsets = tuple(campos)
 
 admin.site.register(Filme)
 admin.site.register(Episodio)
+admin.site.register(Usuario, UserAdmin)
