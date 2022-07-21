e74e70e7c49b6bec37f7fc1e9908ddd3ae1b1d0e
Denilsonaraujoprogramador <101232538+Denilsonaraujoprogramador@users.noreply.github.com>
2022-07-21 16:46:30 -0300

    commit inicial

--- Replica_netflix/settings.py
+++ Replica_netflix/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=vtkokjb^p26(rnmxxvv(a8)ecl+y--a%qnly=df+#b59&k)+h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'orkutflix.herokuapp.com']
 
 
 # Application definition
